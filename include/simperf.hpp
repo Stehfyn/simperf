df0869bbdfc63facd18018e9f1a97cfac1f6daa0
Stephen Foster <stephenfostercpp@gmail.com>
2023-06-25 16:36:31 -0700

    Static Log registry

--- include/simperf.hpp
+++ include/simperf.hpp
@@ -12,12 +12,9 @@
 
 namespace simperf
 {
-	struct logger_specification {
-		std::vector<spdlog::sink_ptr> sinks;
-		std::shared_ptr<spdlog::logger> logger;
-	};
+	typedef std::shared_ptr<spdlog::logger> logger;
 
-	typedef std::vector<logger_specification> log_list;
+	typedef std::vector<std::shared_ptr<spdlog::logger>> log_list;
 
 	enum class register_result {
 		failed = 0,
@@ -28,6 +25,11 @@ namespace simperf
 		return rr == static_cast<register_result>(0);
 	}
 
+	inline std::ostream& operator<<(std::ostream& os, const register_result& rr) {
+		os << ((!rr) ? "register_result::failed" : "register_result::ok");
+		return os;
+	}
+
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
 	template<typename T, typename ... Args>
@@ -36,11 +38,6 @@ namespace simperf
 	{
 		return std::make_shared<T>(std::forward<Args>(args)...);
 	}
-
-	//namespace {
-	//	template<typename Arg, typename... Args>
-	//	LogIt_()
-	//}
 
 	class Log {
 	public:
@@ -53,13 +50,13 @@ namespace simperf
 			if (!s_StaticRegistryInitialized.load(std::memory_order_acquire)) {
 				std::unique_lock<std::mutex> guard(s_RegisterLock);
 				if (!s_StaticRegistryInitialized.load(std::memory_order_acquire)) {
-					for (const auto& log_spec : log_list_) {
-						auto result = RegisterLogger(log_spec);
+					for (const auto& logger : log_list_) {
+						auto result = RegisterLogger(logger);
 						if (!result) {
 							return register_result::failed;
 						}
 						else {
-							s_StaticLoggerRegistry.insert({ log_spec.logger->name(), log_spec.logger });
+							s_StaticLoggerRegistry.insert({ logger->name(), logger });
 						}
 					}
 				}
@@ -98,55 +95,15 @@ namespace simperf
 			}
 		}
 
-		template<typename ... Args>
-		static void LogIt(const std::string& logger_name, const type& logger_type, const spdlog::level::level_enum& log_level, Args&&... args) {
-			auto logger = GetLogger(logger_name, logger_type);
-			logger == nullptr ? void(0) : LogTo(logger, log_level, std::forward<Args>(args)...);
-		}
-
 	private:
-		static register_result RegisterLogger(const logger_specification& log_spec) {
+		static register_result RegisterLogger(const logger& log_spec) {
 			try {
-				spdlog::register_logger(log_spec.logger);
+				spdlog::register_logger(log_spec);
 				return register_result::ok;
 			}
 			catch (spdlog::spdlog_ex& e) {
 				std::cerr << e.what() << std::endl;
 				return register_result::failed;
-			}
-		}
-
-		template<typename ... Args>
-		static void LogTo(Ref<spdlog::logger> logger, const spdlog::level::level_enum& log_level, Args&&... args) {
-			using namespace spdlog::level;
-			switch (log_level)
-			{
-			case trace: {
-				logger->trace(std::forward<Args>(args)...);
-				break;
-			}
-			case debug: {
-				logger->debug(std::forward<Args>(args)...);
-				break;
-			}
-			case info: {
-				logger->info(std::forward<Args>(args)...);
-				break;
-			}
-			case warn: {
-				logger->warn(std::forward<Args>(args)...);
-				break;
-			}
-			case err: {
-				logger->error(std::forward<Args>(args)...);
-				break;
-			}
-			case critical: {
-				logger->critical(std::forward<Args>(args)...);
-				break;
-			}
-			default:
-				break;
 			}
 		}
 
@@ -156,6 +113,207 @@ namespace simperf
 		inline static std::unordered_map<std::string, Ref<spdlog::logger>> s_StaticLoggerRegistry;
 	};
 
-#define SP_STATIC_TRACE_(logger_name, log_level, ...) ::simperf::Log::LogIt(logger_name, ::simperf::Log::type::static_logger, log_level, __VA_ARGS__)
-#define SP_DYNAMIC_TRACE_(logger_name, log_level, ...) ::simperf::Log::LogIt(logger_name, ::simperf::Log::type::dynamic_logger, log_level, __VA_ARGS__)
+#define SP_LOG_IT_(logger_name, logger_type, log_level, ...)  \
+	{ \
+		simperf::Ref<spdlog::logger> logger = simperf::Log::GetLogger(logger_name, logger_type); \
+		logger == nullptr ? void(0) : \
+		log_level == ::spdlog::level::trace ? logger->trace(__VA_ARGS__) : \
+		log_level == ::spdlog::level::debug ? logger->debug(__VA_ARGS__) : \
+		log_level == ::spdlog::level::info ? logger->info(__VA_ARGS__) : \
+		log_level == ::spdlog::level::warn ? logger->warn(__VA_ARGS__) : \
+		log_level == ::spdlog::level::err ? logger->error(__VA_ARGS__) : \
+		log_level == ::spdlog::level::critical ? logger->critical(__VA_ARGS__) : \
+		void(0); \
+	} 
+
+#define SP_STATIC_LOG_(logger_name, log_level, ...) SP_LOG_IT_(logger_name, ::simperf::Log::type::static_logger, log_level, __VA_ARGS__)
+#define SP_DYNAMIC_LOG_(logger_name, log_level, ...) SP_LOG_IT_(logger_name, ::simperf::Log::type::dynamic_logger, log_level, __VA_ARGS__)
+
+	using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
+
+	struct ProfileResult
+	{
+		std::string Name;
+
+		FloatingPointMicroseconds Start;
+		std::chrono::microseconds ElapsedTime;
+		std::thread::id ThreadID;
+	};
+
+	struct InstrumentationSession
+	{
+		std::string Name;
+	};
+
+	class Instrumentor
+	{
+	public:
+		Instrumentor(const Instrumentor&) = delete;
+		Instrumentor(Instrumentor&&) = delete;
+
+		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
+		{
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession)
+			{
+				// If there is already a current session, then close it before beginning new one.
+				// Subsequent profiling output meant for the original session will end up in the
+				// newly opened session instead.  That's better than having badly formatted
+				// profiling output.
+				//if (mcctt::core::Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+				//{
+				//	MCCTT_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+				//}
+				InternalEndSession();
+			}
+			m_OutputStream.open(filepath);
+
+			if (m_OutputStream.is_open())
+			{
+				m_CurrentSession = new InstrumentationSession({ name });
+				WriteHeader();
+			}
+			else
+			{
+				//if (mcctt::core::Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+				//{
+				//	MCCTT_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+				//}
+			}
+		}
+
+		void EndSession()
+		{
+			std::lock_guard lock(m_Mutex);
+			InternalEndSession();
+		}
+
+		void WriteProfile(const ProfileResult& result)
+		{
+			std::stringstream json;
+
+			json << std::setprecision(3) << std::fixed;
+			json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
+			json << "\"name\":\"" << result.Name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.ThreadID << ",";
+			json << "\"ts\":" << result.Start.count();
+			json << "}";
+
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession)
+			{
+				m_OutputStream << json.str();
+				m_OutputStream.flush();
+			}
+		}
+
+		static Instrumentor& Get()
+		{
+			static Instrumentor instance;
+			return instance;
+		}
+	private:
+		Instrumentor()
+			: m_CurrentSession(nullptr)
+		{
+		}
+
+		~Instrumentor()
+		{
+			EndSession();
+		}
+
+		void WriteHeader()
+		{
+			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
+			m_OutputStream.flush();
+		}
+
+		void WriteFooter()
+		{
+			m_OutputStream << "]}";
+			m_OutputStream.flush();
+		}
+
+		// Note: you must already own lock on m_Mutex before
+		// calling InternalEndSession()
+		void InternalEndSession()
+		{
+			if (m_CurrentSession)
+			{
+				WriteFooter();
+				m_OutputStream.close();
+				delete m_CurrentSession;
+				m_CurrentSession = nullptr;
+			}
+		}
+	private:
+		std::mutex m_Mutex;
+		InstrumentationSession* m_CurrentSession;
+		std::ofstream m_OutputStream;
+	};
+
+	class InstrumentationTimer
+	{
+	public:
+		InstrumentationTimer(const char* name)
+			: m_Name(name), m_Stopped(false)
+		{
+			m_StartTimepoint = std::chrono::steady_clock::now();
+		}
+
+		~InstrumentationTimer()
+		{
+			if (!m_Stopped)
+				Stop();
+		}
+
+		void Stop()
+		{
+			auto endTimepoint = std::chrono::steady_clock::now();
+			auto highResStart = FloatingPointMicroseconds{ m_StartTimepoint.time_since_epoch() };
+			auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
+
+			Instrumentor::Get().WriteProfile({ m_Name, highResStart, elapsedTime, std::this_thread::get_id() });
+
+			m_Stopped = true;
+		}
+	private:
+		const char* m_Name;
+		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+		bool m_Stopped;
+	};
+
+	namespace InstrumentorUtils {
+
+		template <size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+		};
+
+		template <size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result = {};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+			return result;
+		}
+	}
 }
--- tests/simperf_test.cpp
+++ tests/simperf_test.cpp
@@ -1,28 +1,59 @@
 #include "../include/simperf.hpp"
 
-#define CORE_TRACE(...) SP_STATIC_TRACE_("core", spdlog::level::trace, __VA_ARGS__)
+#define CORE_TRACE(...)		   SP_STATIC_LOG_("core", spdlog::level::trace, __VA_ARGS__)
+#define CORE_DEBUG(...)		   SP_STATIC_LOG_("core", spdlog::level::debug, __VA_ARGS__)
+#define CORE_INFO(...)		   SP_STATIC_LOG_("core", spdlog::level::info, __VA_ARGS__)
+#define CORE_WARN(...)		   SP_STATIC_LOG_("core", spdlog::level::warn, __VA_ARGS__)
+#define CORE_ERROR(...)		   SP_STATIC_LOG_("core", spdlog::level::err, __VA_ARGS__)
+#define CORE_CRITICAL(...)	   SP_STATIC_LOG_("core", spdlog::level::critical, __VA_ARGS__)
 
-void register_core()
+#define CLIENT_TRACE(...)	   SP_STATIC_LOG_("client", spdlog::level::trace, __VA_ARGS__)
+#define CLIENT_DEBUG(...)	   SP_STATIC_LOG_("client", spdlog::level::debug, __VA_ARGS__)
+#define CLIENT_INFO(...)	   SP_STATIC_LOG_("client", spdlog::level::info, __VA_ARGS__)
+#define CLIENT_WARN(...)	   SP_STATIC_LOG_("client", spdlog::level::warn, __VA_ARGS__)
+#define CLIENT_ERROR(...)	   SP_STATIC_LOG_("client", spdlog::level::err, __VA_ARGS__)
+#define CLIENT_CRITICAL(...)   SP_STATIC_LOG_("client", spdlog::level::critical, __VA_ARGS__)
+
+void register_static_example()
 {
-	std::vector<spdlog::sink_ptr> logSinks;
-	logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-	logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("core.log", true));
-	logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-	logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+	std::vector<spdlog::sink_ptr> sinks;
 
-	std::shared_ptr<spdlog::logger> core = std::make_shared<spdlog::logger>("core", begin(logSinks), end(logSinks));
+	sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("core.log", true));
+
+	sinks[0]->set_pattern("%^[%T] %n: %v%$");
+	sinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+	std::shared_ptr<spdlog::logger> core = std::make_shared<spdlog::logger>("core", std::begin(sinks), std::end(sinks));
+	std::shared_ptr<spdlog::logger> client = std::make_shared<spdlog::logger>("client", std::begin(sinks), std::end(sinks));
+
 	core->set_level(spdlog::level::trace);
 	core->flush_on(spdlog::level::trace);
 
-	using ls = simperf::logger_specification;
-	ls core_spec{ logSinks, core };
+	client->set_level(spdlog::level::trace);
+	client->flush_on(spdlog::level::trace);
 
-	simperf::Log::RegisterStatic({ core_spec });
+	assert(simperf::Log::RegisterStatic({ core, client }) == simperf::register_result::ok);
 }
 
 int main()
 {
-	register_core();
-	CORE_TRACE("hi from core {0}", "0");
+	register_static_example();
+	int x = 0;
+
+	CORE_TRACE("hi from core {0}", x);
+	CORE_DEBUG("hi from core {0}", x);
+	CORE_INFO("hi from core {0}", x);
+	CORE_WARN("hi from core {0}", x);
+	CORE_ERROR("hi from core {0}", x);
+	CORE_CRITICAL("hi from core {0}", x);
+
+	CLIENT_TRACE("hi from client {0}", x);
+	CLIENT_DEBUG("hi from client {0}", x);
+	CLIENT_INFO("hi from client {0}", x);
+	CLIENT_WARN("hi from client {0}", x);
+	CLIENT_ERROR("hi from client {0}", x);
+	CLIENT_CRITICAL("hi from client {0}", x);
+
 	return 0;
 }
