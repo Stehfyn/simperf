22da431b18a591ea2e497bb41d14e60c847a3179
Stephen Foster <stephenfostercpp@gmail.com>
2023-06-30 17:16:28 -0700

    mOdErN cPp!!
    woww

--- include/simperf2.hpp
+++ include/simperf2.hpp
@@ -1,9 +1,13 @@
 #pragma once
-
+#include <string>
+#include <any>
 #include <mutex>
 #include <memory>
 #include <filesystem>
-
+#include <iostream>
+#include <sstream>
+#include <type_traits>
+#include <format>
 #if defined(SIMPERF_LIB)
 	//#include <spdlog/spdlog.h>
 	//#include <spdlog/common.h>
@@ -22,6 +26,7 @@
 
 namespace simperf
 {
+#define SIMPERF_STRINGIFY_MACRO(x) #x
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
 	template<typename T, typename ... Args>
@@ -76,4 +81,305 @@ namespace simperf
 
 		spdlog_setup::from_file(path); //
 	}
+
+	// Helper function to extract the type name as a string
+	template <typename T>
+	constexpr const char* type_name() {
+		std::string_view name, prefix, suffix;
+#ifdef __clang__
+		name = __PRETTY_FUNCTION__;
+		prefix = "const char* openai::type_name() [T = ";
+		suffix = "]";
+#elif defined(__GNUC__)
+		name = __PRETTY_FUNCTION__;
+		prefix = "constexpr const char* openai::type_name() [with T = ";
+		suffix = "]";
+#elif defined(_MSC_VER)
+		name = __FUNCSIG__;
+		prefix = "const char *__cdecl openai::type_name<";
+		suffix = ">(void)";
+#endif
+		name.remove_prefix(prefix.size());
+		name.remove_suffix(suffix.size());
+		return name.data();
+	}
+
+	template <typename T, typename = void>
+	struct HasStreamOperator : std::false_type {};
+
+	template <typename T>
+	struct HasStreamOperator<T, std::void_t<decltype(std::declval<std::ostream&>() << std::declval<T>())>> : std::true_type {};
+
+	struct ProfiledArgBase
+	{
+		virtual ~ProfiledArgBase() {}
+	public:
+		virtual std::string to_string() = 0;
+	};
+
+	template<class T>
+	struct ProfiledArg : ProfiledArgBase
+	{
+		T& _value;
+		static_assert(HasStreamOperator<T>::value, "T must have the << operator defined");
+	public:
+		ProfiledArg(T& v) : _value(v) {}
+		virtual std::string to_string() override {
+			return std::format("{}", _value);
+		}
+	};
+
+	using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
+
+	struct ProfileResult
+	{
+		std::string Name;
+
+		FloatingPointMicroseconds Start;
+		std::chrono::microseconds ElapsedTime;
+		std::thread::id ThreadID;
+	};
+
+	struct InstrumentationSession
+	{
+		std::string Name;
+	};
+
+	class Instrumentor
+	{
+	public:
+		Instrumentor(const Instrumentor&) = delete;
+		Instrumentor(Instrumentor&&) = delete;
+
+		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
+		{
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession)
+			{
+				// If there is already a current session, then close it before beginning new one.
+				// Subsequent profiling output meant for the original session will end up in the
+				// newly opened session instead.  That's better than having badly formatted
+				// profiling output.
+				//if (mcctt::core::Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+				//{
+				//	MCCTT_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+				//}
+				InternalEndSession();
+			}
+			m_OutputStream.open(filepath);
+
+			if (m_OutputStream.is_open())
+			{
+				m_CurrentSession = new InstrumentationSession({ name });
+				WriteHeader();
+			}
+			else
+			{
+				//if (mcctt::core::Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+				//{
+				//	MCCTT_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+				//}
+			}
+		}
+
+		void EndSession()
+		{
+			std::lock_guard lock(m_Mutex);
+			InternalEndSession();
+		}
+
+		void WriteProfile(const ProfileResult& result)
+		{
+			std::stringstream json;
+
+			json << std::setprecision(3) << std::fixed;
+			json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
+			json << "\"name\":\"" << result.Name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.ThreadID << ",";
+			json << "\"ts\":" << result.Start.count();
+			json << "}";
+
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession)
+			{
+				m_OutputStream << json.str();
+				m_OutputStream.flush();
+			}
+		}
+
+		static Instrumentor& Get()
+		{
+			static Instrumentor instance;
+			return instance;
+		}
+	private:
+		Instrumentor()
+			: m_CurrentSession(nullptr)
+		{
+		}
+
+		~Instrumentor()
+		{
+			EndSession();
+		}
+
+		void WriteHeader()
+		{
+			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
+			m_OutputStream.flush();
+		}
+
+		void WriteFooter()
+		{
+			m_OutputStream << "]}";
+			m_OutputStream.flush();
+		}
+
+		// Note: you must already own lock on m_Mutex before
+		// calling InternalEndSession()
+		void InternalEndSession()
+		{
+			if (m_CurrentSession)
+			{
+				WriteFooter();
+				m_OutputStream.close();
+				delete m_CurrentSession;
+				m_CurrentSession = nullptr;
+			}
+		}
+	private:
+		std::mutex m_Mutex;
+		InstrumentationSession* m_CurrentSession;
+		std::ofstream m_OutputStream;
+	};
+
+	class InstrumentationTimer
+	{
+	public:
+		template<typename ...Args>
+		InstrumentationTimer(const char* name, Args&&... args)
+			: m_Name(name), m_Stopped(false)
+		{
+			m_StartTimepoint = std::chrono::steady_clock::now();
+			AddArgs(std::forward<Args>(args)...);
+		}
+
+		~InstrumentationTimer()
+		{
+			if (!m_Stopped)
+				Stop();
+		}
+
+		template <typename ...Args>
+		void AddArgs(Args&& ... inputs) {
+			int i = 0;
+			auto logger = spdlog::get("test");
+			([&]
+				{
+					++i;
+					logger->trace("Arg: {0} val: {1}", i, inputs);
+					m_ProfiledArgs.insert({ 
+						std::to_string(i), 
+						std::make_shared<ProfiledArg<typename std::remove_reference<decltype(inputs)>::type>>(inputs)});
+				} (), ...);
+		}
+
+		void Stop()
+		{
+			auto endTimepoint = std::chrono::steady_clock::now();
+			auto highResStart = FloatingPointMicroseconds{ m_StartTimepoint.time_since_epoch() };
+			auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
+
+			auto logger = spdlog::get("test");
+			//logger->trace("{0} on thread {1} took {2}", m_Name, std::this_thread::get_id(), elapsedTime);
+			//SIMPERF_LOG_PROFILE("{0} on thread {1} took {2}", m_Name, std::this_thread::get_id(), elapsedTime);
+			Instrumentor::Get().WriteProfile({ m_Name, highResStart, elapsedTime, std::this_thread::get_id() });
+			m_Stopped = true;
+			std::stringstream ss;
+			ss << std::this_thread::get_id();
+			uint64_t id = std::stoull(ss.str());
+			logger->trace("{0} on thread {1} took {2}", m_Name, id, elapsedTime);
+			for (const auto& kv : m_ProfiledArgs) {
+
+				logger->trace("Arg: {0} val: {1}", kv.first, kv.second->to_string());
+			}
+		}
+
+	private:
+		const char* m_Name;
+		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+		std::map<std::string, std::shared_ptr<ProfiledArgBase>> m_ProfiledArgs;
+		bool m_Stopped;
+	};
+
+	namespace InstrumentorUtils {
+
+		template <size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+		};
+
+		template <size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result = {};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+			return result;
+		}
+	}
+
+#if (defined(_DEBUG) && !defined(SIMPERF_DISABLE)) || defined(SIMPERF_ENABLE)
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+	#define SIMPERF_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+	#define SIMPERF_FUNC_SIG __PRETTY_FUNCTION__
+	#elif (defined(__FUNCSIG__) || (_MSC_VER))
+	#define SIMPERF_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+	#define SIMPERF_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+	#define SIMPERF_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+	#define SIMPERF_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+	#define SIMPERF_FUNC_SIG __func__
+	#else
+	#define SIMPERF_FUNC_SIG "SIMPERF_FUNC_SIG unknown!"
+	#endif
+
+#define SIMPERF_PROFILE_BEGIN_SESSION(name, filepath) ::simperf::Instrumentor::Get().BeginSession(name, filepath)
+#define SIMPERF_PROFILE_END_SESSION() ::simperf::Instrumentor::Get().EndSession()
+#define SIMPERF_PROFILE_SCOPE_LINE2(name, line, ...) constexpr auto fixedName##line = ::simperf::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+												   ::simperf::InstrumentationTimer timer##line(fixedName##line.Data, __VA_ARGS__)
+
+#define SIMPERF_PROFILE_SCOPE_LINE(name, line, ...) SIMPERF_PROFILE_SCOPE_LINE2(name, line, __VA_ARGS__)
+#define SIMPERF_PROFILE_SCOPE(name, ...) SIMPERF_PROFILE_SCOPE_LINE(name, __LINE__, __VA_ARGS__)
+#define SIMPERF_PROFILE_FUNCTION(...) SIMPERF_PROFILE_SCOPE(SIMPERF_FUNC_SIG, __VA_ARGS__)
+
+#else
+#define SIMPERF_PROFILE_BEGIN_SESSION(name, filepath)
+#define SIMPERF_PROFILE_END_SESSION()
+#define SIMPERF_PROFILE_SCOPE(name)
+#define SIMPERF_PROFILE_FUNCTION()
+#endif
 }
--- tests/simperf2_test.cpp
+++ tests/simperf2_test.cpp
@@ -1,23 +1,73 @@
 #include "../include/simperf2.hpp"
+#include <mutex>
+#include <condition_variable>
 
 void test_initialize() {
 	::simperf::initialize_from_config("log.conf");
 }
 
-int main() {
-	test_initialize();
+std::mutex m;
+std::condition_variable cv;
+std::string data;
+bool ready = false;
+bool processed = false;
 
-	auto logger = spdlog::get("test");
-	logger->trace("Hello World!");
-	logger->debug("Hello World!");
-	logger->info("Hello World!");
-	logger->warn("Hello World!");
-	logger->error("Hello World!");
-	logger->critical("Hello World!");
+void worker_thread()
+{
+    using namespace std::chrono_literals;
+    
+    SIMPERF_PROFILE_FUNCTION(ready, processed, data);
 
-	//::simperf::initialize_from_config()
-	//or
-	//::simperf::ctx::RegisterStaticLoggers(log_specs)
+    auto logger = spdlog::get("test");
 
-	return 0;
+    std::this_thread::sleep_for(3s);
+
+    // Wait until main() sends data
+    std::unique_lock lk(m);
+    cv.wait(lk, [] {return ready; });
+
+    // after the wait, we own the lock.
+    //logger->trace("Worker thread is processing data");
+
+    data += " after processing";
+
+    // Send data back to main()
+    processed = true;
+    //logger->trace("Worker thread signals data processing completed");
+
+    // Manual unlocking is done before notifying, to avoid waking up
+    // the waiting thread only to block again (see notify_one for details)
+    lk.unlock();
+    cv.notify_one();
+}
+
+int main()
+{
+    SIMPERF_PROFILE_BEGIN_SESSION("test", "test-session.json");
+
+    test_initialize();
+
+    std::thread worker(worker_thread);
+    auto logger = spdlog::get("test");
+
+    data = "Example data";
+    // send data to the worker thread
+    {
+        std::lock_guard lk(m);
+        ready = true;
+        logger->trace("main() signals data ready for processing");
+    }
+    cv.notify_one();
+
+    // wait for the worker
+    {
+        std::unique_lock lk(m);
+        cv.wait(lk, [] {return processed; });
+    }
+
+    worker.join();
+    logger->trace("Back in main(), data = {0}:", data);
+
+    SIMPERF_PROFILE_END_SESSION();
+    return 0;
 }
